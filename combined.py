4c64ba1dc00b693514cedfd132b13203c3e7c53f
jorjilour <jorjilour@student.unimelb.edu.au>
2017-05-23 23:15:38 +1000

    Add files via upload

--- combined.py
+++ combined.py
@@ -963,7 +963,8 @@ def insights():
             });
 
     ''' % (str(sorted_list(data_dict2['F'].items())), str(sorted_list(data_dict2['M'].items())))
-    #Create chart three, to show the effect of weekend alcohol consumption on no. of failed subjects
+    
+	#Create chart three, to show the effect of weekend alcohol consumption on no. of failed subjects
     data_dict1 = insightgen("Sex", "Weekend Alcohol Consumption", "Number of Failed Subjects")
     html += '''
             $(function(){
@@ -998,10 +999,199 @@ def insights():
 
             });
     ''' % (str(sorted_list(data_dict1['F'].items())), str(sorted_list(data_dict1['M'].items())))
-
+    
+    data_dict3 = insightgen("Sex", "Quality of Family Relationships", "Weekend Alcohol Consumption")
+    data_dict4 = insightgen("Parent Cohabitation Status", "Sex", "Average Grade")
     html += '''
+			$(function(){
+				var myChart = Highcharts.chart('chart4', {
+					chart: {
+						type: 'bar'
+					},
+					
+					title: {
+						text: 'Quality of Family Relationship vs Weekend Alcohol Consumption'
+					},
+						
+					xAxis: {
+						title: {
+							text: 'Quality of Family Relationships'
+						},
+						categories: [1, 2, 3, 4, 5]
+					},
+						
+					yAxis: {
+						min: 0,
+						title: {
+							text: 'Level of Weekend Alcohol Consumption'
+						}
+					},
+					
+					legend: {
+						reversed: true
+					},
+					
+					plotOptions: {
+						series: {
+							stacking: 'normal'
+						}
+					},
+					
+					series: [{
+						name: 'Males',
+						data: %s
+					}, {
+						name: 'Females',
+						data: %s
+					}]
+				});
+			});
+	'''%(str(sorted_list(data_dict3['M'].items())), str(sorted_list(data_dict3['F'].items())))
+    
+    html += '''
+			$(function(){
+				var myChart = Highcharts.chart('chart5', {
+					chart: {
+						type: 'column'
+					},
+					title: {
+						text: 'Parent Cohabitation Status vs Average Grade'
+					},
+					xAxis: {
+						title: {
+							text: 'Sex'
+						},
+						categories: ['M', 'F'],
+						crosshair: true
+					},
+					yAxis: {
+						min: 0,
+						title: {
+							text: 'Average Grade'
+						}
+					},
+					tooltip: {
+						headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
+						pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
+							'<td style="padding:0"><b>{point.y:.1f} </b></td></tr>',
+						footerFormat: '</table>',
+						shared: true,
+						useHTML: true
+					},
+					plotOptions: {
+						column: {
+							pointPadding: 0.2,
+							borderWidth: 0
+						}
+					},
+					series: [{
+						name: 'Apart',
+						data: %s
+				
+					}, {
+						name: 'Together',
+						data: %s
+				
+					}]
+				});
+			});
+	''' % (str(sorted_list(data_dict4['A'].items())), str(sorted_list(data_dict4['T'].items())))
+    
+    data_dict5 = insightgen("Sex", "Weekend Alcohol Consumption", "Study time")
+    data_dict6 = insightgen("Sex", "Weekday Alcohol Consumption", "Study time")
+    html += '''
+            $(function(){
+                var myChart = Highcharts.chart('chart6', {
+
+                    title: {
+                        text: 'Weekend'
+                    },
+                    
+                    xAxis: {
+                        categories: [1,2,3,4,5],
+                        title: {
+                            text: 'Level of Alcohol Consumption'
+                        }
+                    },
+                
+                    yAxis: {
+                        title: {
+                            text: 'Study Time'
+                        }
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle'
+                    },
+                
+                    plotOptions: {
+                        series: {
+                            pointStart: 0
+                        }
+                    },
+                
+                    series: [{
+                        name: 'Females',
+                        data: %s
+                    }, {
+                        name: 'Males',
+                        data: %s
+                    }]
+                
+                });
+            });
+    ''' % (str(sorted_list(data_dict5['F'].items())), str(sorted_list(data_dict5['M'].items())))
+    
+    html += '''
+            $(function(){
+                var myChart = Highcharts.chart('chart7', {
+
+                    title: {
+                        text: 'Weekday'
+                    },
+                    
+                    xAxis: {
+                        categories: [1,2,3,4,5],
+                        title: {
+                            text: 'Level of Alcohol Consumption'
+                        }
+                    },
+                
+                    yAxis: {
+                        title: {
+                            text: 'Study Time'
+                        }
+                    },
+                    legend: {
+                        layout: 'vertical',
+                        align: 'right',
+                        verticalAlign: 'middle'
+                    },
+                
+                    plotOptions: {
+                        series: {
+                            pointStart: 0
+                        }
+                    },
+                
+                    series: [{
+                        name: 'Females',
+                        data: %s
+                    }, {
+                        name: 'Males',
+                        data: %s
+                    }]
+                
+                });
+            });
+    ''' % (str(sorted_list(data_dict6['F'].items())), str(sorted_list(data_dict6['M'].items())))
+
+    
         
-    </script>
+	
+    html += '''
+        </script>
     </head>
     <body>
     <div id="topbar">
@@ -1045,6 +1235,55 @@ def insights():
         </div>
         <div id="chart3" class="chart"></div>
     </div>
+    
+    <div class="discussion">
+        <div class="text">
+            <h2>Quality of Family Relationships vs Weekend Alcohol Consumption</h2>
+            <p>
+            The graph tells us that as the quality of family relationships decreases for males,
+            the level of their alcohol consumption will most likely increase.
+            </br></br>
+            On the other hand, this decreasing trend is not evident for females.
+            </br></br>
+            Therefore, it can be concluded that a females' alcohol consumption is not affected by their
+            relationship with their family.
+            </p>
+        </div>
+        <div id="chart4" class="chart"></div>
+    </div>
+    
+    <div class="discussion">
+        <div class="text">
+            <h2>Parent's Cohabitation Status vs Average Grade</h2>
+            <p>
+            This demonstrates similar average grades in males, regardless
+            of their parents' cohabitation status.
+            </br> </br>
+            Therefore, male youths' grades will not be affected by their 
+            parent's cohabitation status.
+            </br></br>
+			However, it is observed that females will have a higher average grade
+			when their parents are apart than when they're together
+            </p>
+        </div>
+        <div id="chart5" class="chart"></div>
+    </div>
+    
+    <div class="discussion">
+        <div class="text">
+            <h2>Weekend/Weekday Alcohol Consumption vs Study Time</h2>
+            <p>
+            Overall, as the student's alcohol consumption increases during the
+            weekend or weekdays, their study time decreases.
+            </br></br>
+            However, male students' study time is not affected as much during
+            the weekdays by the level of alcohol they consume.
+            </p>
+        </div>
+        <div id="chart6" class="chart"></div>
+        <div id="chart7" class="chart"></div>
+    </div>
+    
     </body>
     </html>
     '''
